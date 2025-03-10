93f41edf004a9b0ed8bf8084e66abccf53f78601
dabadema <dabadema7@hotmail.com>
2025-03-10 23:27:04 +0100

    exclude dist

--- dist/controllers/TaskController.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-export declare class TaskController {
-    static createTask: (req: Request, res: Response) => Promise<void>;
-    static getTasksByProjectId: (req: Request, res: Response) => Promise<void>;
-    static getTaskById: (req: Request, res: Response) => Promise<void>;
-    static updateTaskById: (req: Request, res: Response) => Promise<void>;
-    static deleteTaskById: (req: Request, res: Response) => Promise<void>;
-    static updateTaskStatus: (req: Request, res: Response) => Promise<void>;
-}
--- dist/controllers/TeamController.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-export declare class TeamMemberController {
-    static findMemberById: (req: Request, res: Response) => Promise<void>;
-    static getProjectTeam: (req: Request, res: Response) => Promise<void>;
-    static addMemberById: (req: Request, res: Response) => Promise<void>;
-    static removeMemberById: (req: Request, res: Response) => Promise<void>;
-}
