ca4eed5a541fca72390cb3125423e77ffd5ad4fa
dabadema <dabadema7@hotmail.com>
2025-03-11 01:25:27 +0100

    Logs added

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsConfig = void 0;
 exports.corsConfig = {
     origin: function (origin, callback) {
+        console.log('origin:', origin);
         const whitelist = [process.env.FRONTEND_URL];
         if (process.argv[2] === '--api') {
             whitelist.push(undefined); // Allow undefined origin as Thunderclient and Postman
